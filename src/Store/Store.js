4e39633eaa2339ff1883c5d359814935195c9cef
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-03 18:35:11 +0530

    feat: added sidebar width to persistance

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,39 +1,21 @@
-import { createStore, action } from "easy-peasy";
+import { createStore, action, persist } from "easy-peasy";
 
-const Store = createStore({
-  theme: "github",
-  activityItem: "explorer",
+const Store = createStore(
+  persist({
+    theme: "github",
+    activityItem: "explorer",
+    sidebarWidth: 284,
 
-  setTheme: action((state, payload) => {
-    state.theme = payload;
-  }),
-  setActivityItem: action((state, payload) => {
-    state.activityItem = payload;
-  }),
-
-  
-  // todo: [
-  //   { id: 1, text: "Learn React", completed: true },
-  //   { id: 2, text: "Learn Redux", completed: false },
-  //   { id: 3, text: "Learn React-Redux", completed: false },
-  // ],
-  // addTodo: action((state, payload) => {
-  //   state.todo.push({
-  //     id: payload.id,
-  //     text: payload.text,
-  //     completed: payload.completed,
-  //   });
-  // }),
-  // removeTodo: action((state, payload) => {
-  //   state.todo = state.todo.filter((todo) => todo.id !== payload);
-  // }),
-  // toggleTodo: action((state, payload) => {
-  //   state.todo.map((item) => {
-  //     if (item.id === payload) {
-  //       item.completed = !item.completed;
-  //     }
-  //   });
-  // }),
-});
+    setTheme: action((state, payload) => {
+      state.theme = payload;
+    }),
+    setActivityItem: action((state, payload) => {
+      state.activityItem = payload;
+    }),
+    setSidebarWidth: action((state, payload) => {
+      state.sidebarWidth = payload;
+    }),
+  })
+);
 
 export { Store };
