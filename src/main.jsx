2419d50191629d1dc8b6ead25b78e47563b135ae
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-03 16:15:27 +0530

    feat: added easy peasy for state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./global.scss";
 import "./config/userWorker";
+import { StoreProvider } from "easy-peasy";
+import { Store } from "./Store/Store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StoreProvider store={Store}>
+    <App />
+  </StoreProvider>
+);
