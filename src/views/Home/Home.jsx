cf7d1dfcbdcca28fd1c576011b61ae6c7ee4151c
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-03 18:35:27 +0530

    feat: working side page UI

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,18 +2,27 @@ import { useStoreState } from "easy-peasy";
 import React from "react";
 import ActivityBar from "../../components/ActivityBar/ActivityBar";
 import MonacoEditor from "../../components/MonacoEditor/MonacoEditor";
+import SidePane from "../../components/SidePane/SidePane";
 import "./Home.scss";
 
 function Home() {
   const [code, setCode] = React.useState("");
   const currentTheme = useStoreState((state) => state.theme);
+  const currentActivity = useStoreState((state) => state.activityItem);
+  const sidebarWidth = useStoreState((state) => state.sidebarWidth);
 
   React.useEffect(() => {
     console.log(code);
   }, [code]);
   return (
-    <div className={`Workbench ${currentTheme}`}>
+    <div
+      className={`Workbench ${currentTheme}`}
+      style={{
+        gridTemplateColumns: `48px ${sidebarWidth}px 1fr`,
+      }}
+    >
       <ActivityBar />
+      {currentActivity !== null && <SidePane />}
       {/* <MonacoEditor setCode={setCode} code={code} /> */}
     </div>
   );
