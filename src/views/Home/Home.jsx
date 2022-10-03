5a3215dad9e56da33e21f43759652edd25dea84c
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-03 16:16:49 +0530

    feat: added Activity bar to home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,16 +1,20 @@
+import { useStoreState } from "easy-peasy";
 import React from "react";
+import ActivityBar from "../../components/ActivityBar/ActivityBar";
 import MonacoEditor from "../../components/MonacoEditor/MonacoEditor";
 import "./Home.scss";
 
 function Home() {
   const [code, setCode] = React.useState("");
+  const currentTheme = useStoreState((state) => state.theme);
 
   React.useEffect(() => {
     console.log(code);
   }, [code]);
   return (
-    <div className="HomeWrapper">
-      <MonacoEditor setCode={setCode} code={code} />
+    <div className={`Workbench ${currentTheme}`}>
+      <ActivityBar />
+      {/* <MonacoEditor setCode={setCode} code={code} /> */}
     </div>
   );
 }
