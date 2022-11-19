807dc942b0a5ca69ccf9f8a40bee27ab05564b8a
ABSanthosh <a.b.santhosh02@gmail.com>
2022-11-19 20:52:02 +0530

    feat: added Dijkstra algorithm

--- src/components/LeftSidePane/VizLayout/VizLayout.jsx
+++ src/components/LeftSidePane/VizLayout/VizLayout.jsx
@@ -1,6 +1,12 @@
 import { useStoreState } from "easy-peasy";
 import { useState } from "react";
+import {
+  dijkstra,
+  getInitialGrid,
+  getNodesInShortestPathOrder,
+} from "../../../utils/VisualizerUtils/GridUtils";
 import { getMergeSortAnimations } from "../../../utils/VisualizerUtils/SortHelper";
+import DijkstraGrid from "../components/DijkstraGrid/DijkstraGrid";
 import SortBlocks from "../components/SortBlocks/SortBlocks";
 import "./VizLayout.scss";
 
@@ -8,6 +14,10 @@ function VizLayout() {
   const vizItem = useStoreState((state) => state.vizItem);
   const PRIMARY_COLOR = "aqua";
   const SECONDARY_COLOR = "red";
+  const START_NODE_ROW = 10;
+  const START_NODE_COL = 9;
+  const FINISH_NODE_ROW = 10;
+  const FINISH_NODE_COL = 30;
 
   const randomHeights = (min, max) =>
     Math.floor(Math.random() * (max - min + 1) + min);
@@ -24,6 +34,7 @@ function VizLayout() {
   const [arraySize, setArraySize] = useState(70);
   const [blockArray, setBlockArray] = useState(generateBlockArray());
 
+  //=================== SORTING ===================
   function mergeSort() {
     const animations = getMergeSortAnimations(blockArray);
     for (let i = 0; i < animations.length; i++) {
@@ -47,6 +58,44 @@ function VizLayout() {
         }, i * animationSpeed);
       }
     }
+  }
+
+  // =================== PATHFINDING ===================
+
+  const [basicGrid, setBasicGrid] = useState(getInitialGrid());
+  function animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`NodeItem-${node.row}-${node.col}`).className =
+          "NodeItem NodeItem--visited";
+      }, 10 * i);
+    }
+  }
+
+  function animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`NodeItem-${node.row}-${node.col}`).className =
+          "NodeItem NodeItem--shortest-path";
+      }, 50 * i);
+    }
+  }
+
+  function visualizeDijkstra() {
+    const grid = basicGrid;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   }
 
   return (
@@ -93,7 +142,14 @@ function VizLayout() {
         </>
       )}
 
-      {vizItem === "path" && <div className="VizLayout__pathBox">Dijkstra</div>}
+      {vizItem === "path" && (
+        <div className="VizLayout__pathBox">
+          <DijkstraGrid basicGrid={basicGrid} setBasicGrid={setBasicGrid} />
+          <button onClick={visualizeDijkstra}>
+            Visualize Dijkstra's Algorithm
+          </button>
+        </div>
+      )}
     </div>
   );
 }
